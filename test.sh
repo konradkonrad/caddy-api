09ef01dc79e27731e0a14f2375761f1e7c9ea77a
Konrad Feldmeier <konrad@brainbot.com>
2025-05-06 17:15:23 +0200

    Test rate limiting

---
+++ test.sh
@@ -0,0 +1,11 @@
+for _ in {0..10};
+do
+    curl -w "%{http_code}\n" http://localhost:8080
+    sleep .1s
+done
+
+for _ in {0..10};
+do
+    curl -w "%{http_code}\n" http://localhost:8080/api
+    sleep .2s
+done
