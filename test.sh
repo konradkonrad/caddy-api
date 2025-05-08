2e56a96d9a09d8b8ddac055bf9d4e8be45227705
Konrad Feldmeier <konrad@brainbot.com>
2025-05-08 17:18:19 +0200

    Configured per-bearer limits

--- test.sh
+++ test.sh
@@ -1,8 +1,15 @@
+PATTERN=" [%{http_code}] %{local_ip} %{url.path}"
+
+ts() {
+  printf $(date +"%T.%3N")' '
+}
+
 printf "Testing free resources\n========\n"
 echo "http://localhost:8080"
 for _ in {0..3};
 do
-    curl -w " %{http_code}\n" http://localhost:8080
+    ts
+    curl --ipv4 -w "${PATTERN} \n" http://localhost:8080
     sleep .01s
 done
 
@@ -11,7 +18,8 @@ printf "\nTesting unauthorized rate limits: max per host 2/1s 4/10s global 10/1m
 echo "http://localhost:8080/api"
 for _ in {0..20};
 do
-    curl -w " %{http_code}\n" http://localhost:8080/api
+    ts
+    curl -w "${PATTERN} \n" http://localhost:8080/api
     sleep .4s
 done
 
@@ -19,22 +27,25 @@ printf "\nTesting unauthorized global rate limits:\n========\n"
 echo "http://localhost:8080/api (different host every time)"
 for _ in {0..6};
 do
-    docker run --rm -it --network testcaddy curlimages/curl -w " %{http_code}\n" http://testcaddy:8080/api
+    ts
+    docker run --rm -it --network testcaddy curlimages/curl -w "${PATTERN} \n" http://testcaddy:8080/api
     sleep .4s
 done
 
 printf "\nTesting authorized rate limits: 10/1s @cheap resource\n========\n"
-echo "-H 'Authorization: Bearer key1' http://localhost:8080/api"
 for _ in {0..20};
 do
-    curl -H "Authorization: Bearer key1" -w " %{http_code}\n" http://localhost:8080/api
+    ts
+    curl -H "Authorization: Bearer key1" -w "${PATTERN} \n" http://localhost:8080/api
     sleep .05s
 done
 
 printf "\nTesting authorized rate limits: 1/1s @expensive resource\n========\n"
-echo "-H 'Authorization: Bearer key1' http://localhost:8080/api/an_expensive_resource"
 for _ in {0..20};
 do
-    curl -H "Authorization: Bearer key1" -w " %{http_code}\n" http://localhost:8080/api/an_expensive_resource
+    ts
+    curl -H "Authorization: Bearer key1" -w "${PATTERN} key1 \n" http://localhost:8080/api/an_expensive_resource
+    ts
+    curl -H "Authorization: Bearer key2" -w "${PATTERN} key2 \n" http://localhost:8080/api/an_expensive_resource
     sleep .05s
 done
