afe44e5dd540a8ce2b2934c83f93ae42e05bc90a
Konrad Feldmeier <konrad@brainbot.com>
2025-05-08 14:04:06 +0200

    Auto format Caddyfile

--- run.sh
+++ run.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
-mkdir -p data/caddy/rate_limit
+mkdir -p data/caddy/rate_limit data/caddy/keys
 
-docker run --rm -it -v $(pwd)/Caddyfile:/etc/caddy/Caddyfile:ro -p 8080:8080 -v data:/data caddy-api /usr/bin/caddy run -c /etc/caddy/Caddyfile
+openssl ecparam -genkey -name prime256v1 -noout \
+  -out data/caddy/keys/sign_key.pem
+openssl ec -in data/caddy/keys/sign_key.pem -pubout \
+  -out data/caddy/keys/verify_key.pem
+
+ls data/caddy/keys
+
+docker run --rm -it \
+    -v $(pwd)/Caddyfile:/etc/caddy/Caddyfile \
+    caddy-api /usr/bin/caddy fmt --overwrite /etc/caddy/Caddyfile
+
+docker run --rm -it \
+    -v $(pwd)/Caddyfile:/etc/caddy/Caddyfile:ro \
+    -p 8080:8080 \
+    -v data:/data \
+    -e VERIFY_KEY_DIR=/data/caddy/keys \
+    caddy-api /usr/bin/caddy run -c /etc/caddy/Caddyfile
 
 rm -r data
