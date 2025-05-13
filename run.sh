d8fcc2ee7e77e126a940129c601332149508e324
Konrad Feldmeier <konrad@brainbot.com>
2025-05-13 13:46:48 +0200

    Add script for key handling

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ mkdir -p data/caddy/{locks,rate_limit/instances}
 docker run --rm -it \
     -v $(pwd)/Caddyfile:/etc/caddy/Caddyfile \
     -v $(pwd)/apikeys:/etc/caddy/apikeys \
-    -v $(pwd)/users:/etc/caddy/users \
     caddy-api /usr/bin/caddy fmt --overwrite /etc/caddy/Caddyfile
 
 docker network create testcaddy
@@ -18,7 +17,6 @@ docker run --rm -it \
     --network-alias testcaddy \
     -v $(pwd)/Caddyfile:/etc/caddy/Caddyfile \
     -v $(pwd)/apikeys:/etc/caddy/apikeys \
-    -v $(pwd)/users:/etc/caddy/users \
     -p 8080:8080 \
     -v $(pwd)/data:/data \
     caddy-api /usr/bin/caddy run -c /etc/caddy/Caddyfile
